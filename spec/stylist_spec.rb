18aeabfb2c1e1d5f7abac64960f8aff231d16bbd
Isaac Wong <isaacwongbusiness@gmail.com>
2017-04-05 23:25:06 +0800

    finished michael jackson client backend

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -56,15 +56,15 @@ describe("#delete") do
   end
 end
 
-# describe('#clients') do
-#   it('returns an array of clients for that stylist') do
-#     @test_stylist.save()
-#     test_client = Client.new((:id=>nil, :name=>"Client 1", :stylist_id=>1))
-#     test_client.save()
-#     test_client2 = Client.new((:id=>nil, :name=>"Client 1"), :stylist_id=>1))
-#     test_client2.save()
-#     expect(@test_stylist.clients()).to(eq([test_client, test_client2]))
-#   end
-# end
+describe('#clients') do
+  it('returns an array of clients for that stylist') do
+    @test_stylist.save()
+    test_client = Client.new({:id=>nil, :name=>"Client 1 Jeff", :stylist_id=>@test_stylist.id()})
+    test_client.save()
+    test_client2 = Client.new({:id=>nil, :name=>"Client 2 Michael Jackson", :stylist_id=>@test_stylist.id()})
+    test_client2.save()
+    expect(@test_stylist.clients()).to(eq([test_client, test_client2]))
+  end
+end
 
 end
